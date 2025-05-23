008e195a4f4141718c3053f03f546a5b63982dfd
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-05-23 15:56:06 +0800

    Update SetupBanners.js

--- UI/banners/SetupBanners.js
+++ UI/banners/SetupBanners.js
@@ -1,5 +1,5 @@
 const setupBanners = {
-    helpBanner : 'https://i.ibb.co/23C0zG31/standard-1.gif',
+    helpBanner : 'https://cdn.discordapp.com/attachments/1181603235237216286/1375380031009722380/fkyozUr.gif?ex=683179f8&is=68302878&hm=f2051a0d83ebedcd68054f550abb42735292bff26114a2ef29fb472b57087fbc&',
     verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
     ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
     autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
